24d9452ddba4dccd31e73e1dad9f1d7113c87d0e
Nahuel De La Cruz <n_p4nic@hotmail.com>
2024-08-06 14:48:45 -0300

    Implementación de servicio de tickets en controlador

--- src/presentation/services/ticket.service.ts
+++ src/presentation/services/ticket.service.ts
@@ -2,7 +2,7 @@ import { UuidAdapter } from "../../config/uuid.adapter";
 import { Ticket } from "../../domain/interfaces/ticket";
 
 export class TicketService {
-  private readonly tickets: Ticket [] = [
+  public readonly tickets: Ticket [] = [
     {id: UuidAdapter.v4(),number: 1,createdAt: new Date(), done: false},
     {id: UuidAdapter.v4(),number: 2,createdAt: new Date(), done: false},
     {id: UuidAdapter.v4(),number: 3,createdAt: new Date(), done: false},
@@ -10,4 +10,60 @@ export class TicketService {
     {id: UuidAdapter.v4(),number: 5,createdAt: new Date(), done: false},
     {id: UuidAdapter.v4(),number: 6,createdAt: new Date(), done: false},
   ];
-}
+
+  private readonly workingOnTickets: Ticket[] = [];
+
+  public get pendingTickets():Ticket[]{
+    return this.tickets.filter((ticket)=>!ticket.handleAtDesk && !ticket.done);
+  };
+  public get lastWorkingOnTickets():Ticket[]{
+    return this.workingOnTickets.slice(0,4);
+  }
+  public get lastTicketNumber(): number{
+    return this.tickets.length > 0 ? this.tickets.at(-1)!.number : 0; 
+  };
+
+  public createTicket(): Ticket{
+    const newTicket: Ticket = {
+      id: UuidAdapter.v4(),
+      number: this.lastTicketNumber,
+      createdAt: new Date(),
+      done: false,
+      handleAt: undefined,
+      handleAtDesk: undefined,
+    }
+    this.tickets.push(newTicket);
+    // TODO: Ws
+    return newTicket;
+  };
+
+  public drawTicket(desk: string){
+    const ticket = this.tickets.find(t=>!t.handleAtDesk);
+    if(!ticket) return {status: 'Error', message: 'No hay tickets pendientes'};
+    ticket.handleAtDesk = desk;
+    ticket.handleAt = new Date();
+
+    this.workingOnTickets.unshift({...ticket});
+
+    // TODO: WS (Ahora hay un usuario trabajando con el ticket)
+
+    return {status: 'ok', ticket};
+  };
+
+  public onFinishTicket(id: string){
+    const ticket = this.tickets.find(t=>t.id === id);
+    if(!ticket) return {status: 'error', message: 'Ticket no encontrado'};
+    this.tickets.map(t=>{
+      if(t.id===id){
+        ticket.done = true;
+        ticket.doneAt = new Date();
+      };
+      return ticket;
+    });
+
+    return {status: 'ok'};
+  };
+
+  
+
+};
--- src/presentation/tickets/controller.ts
+++ src/presentation/tickets/controller.ts
@@ -1,34 +1,39 @@
 import { Request, Response } from 'express';
+import { TicketService } from '../services/ticket.service';
 
 export class TicketController {
   // DI - WssService
-  constructor() {}
+  constructor(
+    private readonly ticketService = new TicketService()
+  ) {}
 
   public getTickets = async (req: Request, res: Response) => {
-    return res.json('getTickets');
+    return res.json(this.ticketService.tickets);
   };
 
   public getLastTicketNumber = async (req: Request, res: Response) => {
-    return res.json('getLastTicketNumber');
+    return res.json(this.ticketService.lastTicketNumber);
   };
 
   public pendingTickets = async (req: Request, res: Response) => {
-    return res.json('pendingTickets');
+    return res.json(this.ticketService.pendingTickets);
   };
 
   public createTicket = async (req: Request, res: Response) => {
-    return res.json('createTicket');
+    return res.status(201).json(this.ticketService.createTicket());
   };
 
   public drawTicket = async (req: Request, res: Response) => {
-    return res.json('drawTicket');
+    const {desk} = req.params;
+    return res.json(this.ticketService.drawTicket(desk));
   };
 
   public ticketFinished = async (req: Request, res: Response) => {
-    return res.json('ticketFinished');
+    const {ticketId} = req.params;
+    return res.json(this.ticketService.onFinishTicket(ticketId));
   };
 
   public workingOn = async (req: Request, res: Response) => {
-    return res.json('workingOn');
+    return res.json(this.ticketService.lastWorkingOnTickets);
   };
 }
